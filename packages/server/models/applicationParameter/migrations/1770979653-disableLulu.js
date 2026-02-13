c77fc3bd9176aa94a82c53ac5f101f3308610947
Grigor Malo <grigormalo@gmail.com>
2026-02-13 12:53:40 +0100

    Disable lulu integration

---
+++ packages/server/models/applicationParameter/migrations/1770979653-disableLulu.js
@@ -0,0 +1,41 @@
+const { logger } = require('@coko/server')
+
+exports.up = async knex => {
+  try {
+    const config = await knex('application_parameter')
+      .select('config')
+      .where('area', 'integrations')
+
+    const configObject = JSON.parse(config[0].config)
+    configObject.lulu.disabled = true
+
+    await knex('application_parameter')
+      .update({
+        config: JSON.stringify(configObject),
+      })
+      .where('area', 'integrations')
+  } catch (error) {
+    logger.error('ApplicationParamer: disabling lulu config failed!')
+    throw new Error(error)
+  }
+}
+
+exports.down = async knex => {
+  try {
+    const config = await knex('application_parameter')
+      .select('config')
+      .where('area', 'integrations')
+
+    const configObject = JSON.parse(config[0].config)
+    configObject.lulu.disabled = false
+
+    await knex('application_parameter')
+      .update({
+        config: JSON.stringify(configObject),
+      })
+      .where('area', 'integrations')
+  } catch (error) {
+    logger.error('ApplicationParamer: enabling lulu config failed!')
+    throw new Error(error)
+  }
+}
